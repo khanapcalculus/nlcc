bae60379a5dcfcf11899e92bcc5af5d32f15ecfe
Your Name <your.email@example.com>
2025-07-10 11:01:49 +0530

    Simplify build configuration and fix module loading

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,11 @@
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/lccrtc/',
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-    },
-  },
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-      output: {
-        assetFileNames: 'assets/[name].[hash][extname]',
-        chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js',
-      },
-    },
-    sourcemap: true,
+    outDir: 'dist'
   },
   server: {
     host: true,
