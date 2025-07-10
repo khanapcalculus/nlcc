183035fcc124ac2f9e6268ad77cf33141781cdc7
khanapcalculus <khan.apcalculus@gmail.com>
2025-07-10 09:51:08 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/rtc-whiteboard/',
+  base: '/lccrtc/',
   server: {
     host: true,
     proxy: {
