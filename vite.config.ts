0a9f6d09fcbb37ab4658fd0272290c4a9b054d4d
Your Name <your.email@example.com>
2025-07-13 13:07:59 +0530

    Update repository with current state - working whiteboard with server

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/lccrtc/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/rtc-whiteboard/' : '/',
   build: {
     outDir: 'dist'
   },
